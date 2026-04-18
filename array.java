7d8c49291c2bd8fe8f59f8d9830fd76662d65c34
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-04-18 23:39:10 +0530

    java

---
+++ array.java
@@ -0,0 +1,18 @@
+
+    import java.util.*;
+class array{
+    public static void main(String[] args) {
+        
+        // Array example
+        int[] arr = {10, 20, 30, 40, 50};
+        System.out.println(Arrays.toString(arr));
+
+        // ArrayList example
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+    }
+}
+
