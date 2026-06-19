0a2b9b0c5b4642c920771f4bb2072fb5a92cdb46
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-06-19 23:48:49 +0530

    hello

---
+++ function.java/function1.java
@@ -0,0 +1,19 @@
+// import java.util.Scanner;
+
+// public class function1 {
+//     public static void  printmyName(String   name ){
+//        System.out.println("heeee");
+//        return ;
+//     }
+//     public static void main(String[]  args){
+//    Scanner sc =new Scanner(System.in);
+    
+// String name= sc.next();
+// printmyName(name);
+//     }
+// }
+
+
+
+
+}
